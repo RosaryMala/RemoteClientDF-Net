3eb14fca56988099cd03a75e9e368ce9291b7f77
JapaMala <japa.mala.illo@gmail.com>
2020-05-02 14:18:59 -0700

    Organize some, but not all, of the enums.

---
+++ DFHack/Enums/SiteRealizationBuildingType.cs
@@ -0,0 +1,38 @@
+namespace DF.Enums
+{
+    public enum SiteRealizationBuildingType
+    {
+        CottagePlot, // 0, 0x0
+        CastleWall, // 1, 0x1
+        CastleTower, // 2, 0x2
+        CastleCourtyard, // 3, 0x3
+        House, // 4, 0x4
+        Temple, // 5, 0x5
+        Tomb, // 6, 0x6
+        ShopHouse, // 7, 0x7
+        Warehouse, // 8, 0x8
+        MarketSquare, // 9, 0x9
+        Pasture, // 10, 0xA
+        Waste, // 11, 0xB
+        Courtyard, // 12, 0xC
+        Well, // 13, 0xD
+        Vault, // 14, 0xE
+        GreatTower, // 15, 0xF
+        Trenches, // 16, 0x10
+        TreeHouse, // 17, 0x11
+        HillockHouse, // 18, 0x12
+        MeadHall, // 19, 0x13
+        FortressEntrance, // 20, 0x14
+        Library, // 21, 0x15
+        Tavern, // 22, 0x16
+        CountingHouse, // 23, 0x17
+        GuildHall, // 24, 0x18
+        CityTower, // 25, 0x19
+        Shrine, // 26, 0x1A
+        Anon1, // 27, 0x1B
+        Dormitory, // 28, 0x1C
+        Dininghall, // 29, 0x1D
+        NecromancerTower, // 30, 0x1E
+        Barrow, // 31, 0x1F
+    };
+}
