b5cac3abd8f93f281ece7907b152b0bdea6f7fb2
JapaMala <japa.mala.illo@gmail.com>
2020-05-02 15:06:49 -0700

    Add improvement type enum

---
+++ DFHack/Enums/ImprovementType.cs
@@ -0,0 +1,19 @@
+namespace DF.Enums
+{
+    enum ImprovementType
+    {
+        ArtImage, // 0, 0x0
+        Covered, // 1, 0x1
+        RingsHanging, // 2, 0x2
+        Bands, // 3, 0x3
+        Spikes, // 4, 0x4
+        Itemspecific, // 5, 0x5
+        Thread, // 6, 0x6
+        Cloth, // 7, 0x7
+        SewnImage, // 8, 0x8
+        Pages, // 9, 0x9
+        Illustration, // 10, 0xA
+        InstrumentPiece, // 11, 0xB
+        Writing, // 12, 0xC
+    };
+}
